4ba44fb2375bf0d66419ad25090d9b471e09191c
AndreFSilveira <andrefelipesilveira@gmail.com>
2021-07-26 23:14:56 -0300

    update gtm

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,20 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <script>
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WV7GHFL');
-        </script>
+         <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WV7GHFL');`}} />
 
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body style={{ margin: 0 }}>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV7GHFL" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV7GHFL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+      }}
+    />
         <main className={styles.main}>
 
           <h1 className={styles.title}>
