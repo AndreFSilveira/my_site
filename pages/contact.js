8319b08884e66eef4335fba6f5fff60c08316759
andre <andre@envixo.com>
2021-08-19 11:05:02 -0300

    Add contact page

---
+++ pages/contact.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Contact() {
+  return (
+    <div className={styles.container}>
+      <Head>
+         <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WV7GHFL');`}} />
+
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body style={{ margin: 0 }}>
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV7GHFL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+      }}
+    />
+        <main className={styles.main}>
+
+          <h1 className={styles.title}>
+            Contato
+          </h1>
+        </main>
+      </body>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
