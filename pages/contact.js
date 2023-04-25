77f229351dfc73bef03e496c63b40f511292af92
AndreFSilveira <andrefelipesilveira@gmail.com>
2023-04-25 08:48:36 -0300

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -5,6 +5,8 @@ export default function Contact() {
   return (
     <div className={styles.container}>
       <Head>
+  <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
+<script>eruda.init();eruda.show();</script>
          <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
