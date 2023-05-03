8dd5a603f6276b8c02f21b17bb73ac84b3fb4b7c
AndreFSilveira <andrefelipesilveira@gmail.com>
2023-05-03 15:05:26 -0300

    Update price.jsx

--- pages/price.jsx
+++ pages/price.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Image from 'next/image';
 import { useState } from 'react';
 import styles from "./precos.module.css";
