1eee77a4c8ee583ce3441da06bed38226511de71
AndreFSilveira <andrefelipesilveira@gmail.com>
2023-05-03 15:25:10 -0300

    Create layout.jsx

---
+++ pages/layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./partials/navbar";
+
+function Layout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
