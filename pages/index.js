f9d4ae0f3dd0d92c33d84895a42338764af44ffd
AndreFSilveira <andrefelipesilveira@gmail.com>
2023-05-03 15:40:39 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Layout from "./layout";
 
 export default function Home() {
 
   return (
+    <Layout>
     <div className={styles.container}>
       <Head>
-
-         <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WV7GHFL');`}} />
-
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -75,5 +70,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
         </a>
       </footer>
     </div>
+</Layout>
   )
 }
