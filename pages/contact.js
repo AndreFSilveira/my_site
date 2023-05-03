a727c3cba6b0cbb7933faf51847a624e086010ef
AndreFSilveira <andrefelipesilveira@gmail.com>
2023-05-03 16:11:51 -0300

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -9,32 +9,30 @@ export default function Contact() {
     <div className={styles.container}>
       <Head>
 
-        <title>Create Next App</title>
+        <title>Entre em Contato | Empresa CRM Imobiliário</title>
+        <meta name="description" content="Entre em contato conosco para obter mais informações sobre nossos serviços para o mercado imobiliário. Preencha o formulário ou envie um e-mail para a nossa equipe de atendimento." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body style={{ margin: 0 }}>
-        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV7GHFL"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-      }}
-    />
-        <main className={styles.main}>
-
-          <h1 className={styles.title}>
-            Contato
-          </h1>
-        </main>
-      </body>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+     <div>
+      <h1 style={{ color: '#333', textAlign: 'center', marginTop: '50px' }}>Contato</h1>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+        <form style={{ width: '50%' }}>
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor="name" style={{ display: 'block', marginBottom: '5px' }}>Nome:</label>
+            <input type="text" id="name" name="name" style={{ width: '100%', padding: '10px', borderRadius: '5px' }} />
+          </div>
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor="email" style={{ display: 'block', marginBottom: '5px' }}>E-mail:</label>
+            <input type="email" id="email" name="email" style={{ width: '100%', padding: '10px', borderRadius: '5px' }} />
+          </div>
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor="message" style={{ display: 'block', marginBottom: '5px' }}>Mensagem:</label>
+            <textarea id="message" name="message" rows="5" style={{ width: '100%', padding: '10px', borderRadius: '5px' }}></textarea>
+          </div>
+          <button type="submit" style={{ backgroundColor: '#333', color: '#fff', padding: '10px 20px', borderRadius: '5px' }}>Enviar</button>
+        </form>
+      </div>
+    </div>
     </div>
 </Layout>
   )
