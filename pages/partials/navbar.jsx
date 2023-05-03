63c6e0c09bf00da9fc78901ef60594d4543dfab7
AndreFSilveira <andrefelipesilveira@gmail.com>
2023-05-03 16:21:36 -0300

    Update navbar.jsx

--- pages/partials/navbar.jsx
+++ pages/partials/navbar.jsx
@@ -9,6 +9,10 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WV7GHFL');`}} />
+<noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV7GHFL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+      }}
+    />
     <nav className={styles.menu}>
       <ul>
         <li>
